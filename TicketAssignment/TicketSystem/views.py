5122cd984a85defe8e022949d0073a099be855a0
Strachan <caitlin.strachan@sage.com>
2022-08-19 16:21:09 +0100

    edit tickets

--- TicketAssignment/TicketSystem/views.py
+++ TicketAssignment/TicketSystem/views.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for,flash
 from TicketSystem import app
-import sqlite3
-import flask_sq
+import sqlite3 as sql
+from flask_sqlalchemy import SQLAlchemy
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///ticket.db'
+
+#initialise the database
+#db = SQLAlchemy(app)
+
+#create db model 
+
 
 def get_db_connection():
-    conn = sqlite3.connect('ticketdb.db')
-    conn.row_factory = sqlite3.Row
+    conn = sql.connect('ticketdb.db')
+    conn.row_factory = sql.Row
     return conn
 
 @app.route("/")
@@ -41,13 +48,29 @@ def activetickets():
     tickets = cursor.fetchall()
     return render_template("activetickets.html", tickets=tickets)
 
-@app.route("/activetickets/edit")
-def editactivetickets():
+@app.route("/activetickets/edit/<string:TicketID>", methods=['POST','GET'])
+def editactivetickets(TicketID):
+    if request.method=='POST':
+        Description=request.form['Description']
+        ProductName=request.form['ProductName']
+        TeamID=request.form['TeamID']
+        ClientBackup=request.form['ClientBackup']
+        Priority=request.form['Priortiy']
+        Status=request.form['Status']
+        #DateRaised=request.form['DateRaised']
+        DateResolved=request.form['DateResolved']       
+        conn = get_db_connection()
+        cursor=conn.cursor()
+        cursor.execute("UPDATE Tickets SET Description=?,ProductName=?,TeamID=?,ClientBackup=?,Priority=?,Status=?,DateResolved=? WHERE TicketID=?", (Description,ProductName,TeamID,ClientBackup,Priority,Status,DateResolved,TicketID))
+        conn.commit()
+        #flash('User Updated','success')
+        return redirect(url_for("activetickets"))
     conn = get_db_connection()
+    conn.row_factory=sql.Row
     cursor = conn.cursor()
-    #ONLY LOAD THOSE WITH NOT A 'DONE' STATUS
-    cursor.execute('SELECT * FROM Tickets WHERE Status NOT LIKE "Done"')
-    tickets = cursor.fetchall()
+    #ONLY LOAD THE SELECTED TICKET TO EDIT
+    cursor.execute('SELECT * FROM Tickets WHERE TicketID=?', TicketID)
+    tickets = cursor.fetchone()
     return render_template("editactivetickets.html", tickets=tickets)
 
 @app.route("/completedtickets")
