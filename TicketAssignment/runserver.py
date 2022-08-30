062ad78cf40d3176926ba21773abc19d9cd90fda
Caitlin Strachan <caitlin.strachan@sage.com>
2022-08-30 15:26:06 +0100

    change port for automatic deployments

--- TicketAssignment/runserver.py
+++ TicketAssignment/runserver.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('PORT', '5555'))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
