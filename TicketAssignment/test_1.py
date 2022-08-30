5f91102fff9baa30afe46bc12388d89ccceb2cc2
Caitlin Strachan <caitlin.strachan@sage.com>
2022-08-30 14:03:25 +0100

    update session ID to be randomly generated and add tests

---
+++ TicketAssignment/test_1.py
@@ -0,0 +1,49 @@
+import unittest
+from TicketSystem import app, views
+from flask import Flask, render_template
+import json
+import sqlite3 as sql
+import os
+import hashlib
+
+
+class Test_test_1(unittest.TestCase):   
+
+    def test_home_route(self):
+        app = Flask(__name__, template_folder="templates")
+        with app.app_context():
+            client = views.home()
+            #client = app.home()
+            url = '/'
+            
+            response = client.get(url)        
+            assert response.status_code == 200
+
+    def test_login(self):
+         app = Flask(__name__, template_folder="templates")        
+         with app.app_context():
+            conn = sql.connect('ticketdb.db')
+            conn.row_factory = sql.Row
+            #views.login()
+            data = {"email":"testadmin1@gmail.com","password":"testpass"}
+            response = views.login().post("/login/", data)
+            assert response.status_code == 200 
+            assert response.json()["email"] == "testadmin1@gmail.com"
+            assert response.json()["logged_in"] == True
+        
+    #def test_addUser(self):
+    #    app = Flask(__name__, template_folder="templates")         
+    #    with app.app_context():
+    #        #load database 
+    #        conn = sql.connect('ticketdb.db')
+    #        conn.row_factory = sql.Row
+    #        #count number of users 
+    #
+    #        #add user 
+    #        #app.newuser()
+    #        #count number 
+    #        #check count has increased 
+    #        assert response.status_code == 200
+    #
+if __name__ == '__main__':
+    unittest.main()
